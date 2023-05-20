b1aac9f3a4067cdaede1c9e7015aa40347376303
Abdiraimov <kuba.abdiraimov.bj@gmail.com>
2023-05-20 18:39:09 +0600

    done

---
+++ sync_async_for_bj/bin/sync_async_for_bj.dart
@@ -0,0 +1,30 @@
+
+
+import 'dart:io';
+
+void main() {
+  resultTasks();
+}
+
+void resultTasks() async{
+  task1();
+  String stringTask2 = await task2();
+  task3(stringTask2);
+}
+
+void task1(){
+  print('Task 1 print');
+}
+Future<String> task2() async{
+  Duration threeSeconds = Duration(seconds: 3);
+  String? result;
+  await Future.delayed(threeSeconds,(){
+     result = 'Task 2 Data';
+    print('Task 2 print');
+  });
+  return result.toString();
+  
+}
+void task3(String task2String){
+  print('Task 3 print : $task2String');
+}
---
+++ sync_async_for_bj/test/sync_async_for_bj_test.dart
@@ -0,0 +1,8 @@
+import 'package:sync_async_for_bj/sync_async_for_bj.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
