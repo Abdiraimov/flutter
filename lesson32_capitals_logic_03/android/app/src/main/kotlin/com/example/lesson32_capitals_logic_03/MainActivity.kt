fca46fa93867ee80f16cfc448f5e91c5ba876259
Abdiraimov <kuba.abdiraimov.bj@gmail.com>
2023-06-19 22:49:31 +0600

    do

---
+++ lesson32_capitals_logic_03/android/app/src/main/kotlin/com/example/lesson32_capitals_logic_03/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lesson32_capitals_logic_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lesson32_capitals_logic_03/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lesson32_capitals_logic_03/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
